f0999e4689637d8c38953e128dddaafe04f902bb
Zhiqin He <110511317+MonoScyron@users.noreply.github.com>
2022-08-03 03:52:01 -0400

    Fixed some funky interactions with Erekir
    In the jankiest way possible.

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,33 @@
 Events.on(ClientLoadEvent, () => {
     let p = Vars.ui.planet;
+    var l;
     p.shown(() => {
-        if(p.mode === PlanetDialog.Mode.look){
+        if (p.mode === PlanetDialog.Mode.look) {
             p.fill(cons(t => {
                 t.top().left();
+                t.margin(10);
                 t.defaults().size(200, 54);
                 t.button("Redirect", Icon.upOpen, () => {
-                    let s = Planets.serpulo.sectors;
-                    p.showSelect(s.get(15), d => {
-                        s.each(e => {
-                            e.info.destination = d;
+                    if (p.state.planet.equals(Planets.serpulo)) {
+                        let s = Planets.serpulo.sectors;
+                        p.showSelect(s.get(15), d => {
+                            s.each(e => {
+                                e.info.destination = d;
+                            });
                         });
-                    });
+                        if (t.getChildren().size > 1) {
+                            t.reset();
+                        }
+                    }
+                    else {
+                        if (t.getChildren().size <= 1) {
+                            t.add("No launchpads in Erekir (yet)", Color.scarlet);
+                        }
+                    }
                 });
+                t.row();
             }));
         }
     });
 });
+
